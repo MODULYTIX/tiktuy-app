22e0d85ba123aba14957296773a99307198b94d2
esaumorales <esaumorales2322@gmail.com>
2025-10-08 23:22:39 -0500

    fixed(registroinvitacon): registro invitacion page

--- src/role/ecommerce/pages/MovimientosPage.tsx
+++ src/role/ecommerce/pages/MovimientosPage.tsx
@@ -31,6 +31,7 @@ export default function RegistroMovimientoPage() {
     almacenamiento_id: '',
     categoria_id: '',
     estado: '',
+    nombre: '',
     stock_bajo: false,
     precio_bajo: false,
     precio_alto: false,
