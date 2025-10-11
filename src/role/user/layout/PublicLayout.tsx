9a2ead94b3e4921b26298ba027cfd8144c4e1fda
esaumorales <esaumorales2322@gmail.com>
2025-10-11 03:55:28 -0500

    add(cmomponent): footer

--- src/role/user/layout/PublicLayout.tsx
+++ src/role/user/layout/PublicLayout.tsx
@@ -1,9 +1,12 @@
+import Footer from '../components/Footer';
+
 // src/shared/layout/PublicLayout.tsx
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
     return (
       <div>
         
         {children}
+        <Footer />
       </div>
     );
   }
